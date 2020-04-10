753edcf951f15ad99fb21134b364e60138d10f7b
Nik Rowe <NikRowe7@Gmail.com>
2020-04-10 12:22:16 -0700

    React Transitions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// COMPONENTS
 import Directory from './DirectoryComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -6,11 +7,19 @@ import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import About from './AboutComponent';
+
+//REACT ROUTER
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+// REDUX
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
 import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+// ANIMATIONS
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
+
+
+// REDUX functions to manage State
 const mapStateToProps = state => {
     return {
         campsites: state.campsites,
@@ -28,9 +37,9 @@ const mapDispatchToProps = {
     fetchPromotions: () => (fetchPromotions())
 };
 
-
+// MAIN holds switch & all other parent components
 class Main extends Component {
-
+    // Redux this assignments
     componentDidMount() {
         this.props.fetchCampsites();
         this.props.fetchComments();
@@ -38,7 +47,7 @@ class Main extends Component {
     }
 
     render() {
-
+        // Initial view pulled from campsites data
         const HomePage = () => {
             return (
                 <Home
@@ -53,6 +62,7 @@ class Main extends Component {
             );
         }
 
+        // This is what displays when you click on a campsite from within Directory
         const CampsiteWithId = ({match}) => {
             return (
                 <CampsiteInfo 
@@ -66,17 +76,22 @@ class Main extends Component {
             );
         };
 
+        // The switch - React Router - this is how the links work for bringing up new pages/views
         return (
             <div>
                 <Header />
-                <Switch>
-                    <Route path='/home' component={HomePage} />
-                    <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
-                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                    <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
-                    <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
-                    <Redirect to='/home' />
-                </Switch>
+                <TransitionGroup>
+                    <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+                        <Switch>
+                            <Route path='/home' component={HomePage} />
+                            <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
+                            <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                           <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
+                            <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
+                            <Redirect to='/home' />
+                        </Switch>
+                    </CSSTransition>
+                </TransitionGroup>
                 <Footer />
             </div>
         );
